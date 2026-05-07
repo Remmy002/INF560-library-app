dcde77bf543c0842f260640abcb3941c0c2083c7
Remmy002 <remmymoreiraquispe@gmail.com>
2026-05-07 11:47:16 -0400

    test: agregar ruta temporal /miembros para probar el componente badge

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,20 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BookController;
+use App\Http\Controllers\AuthorController;
+use App\Http\Controllers\CategoriesController;
+use App\Models\Member;
 
-Route::get('/', function () {
-    return view('welcome');
+// Redirección de la raíz al catálogo de libros
+Route::redirect('/', '/books');
+
+// Rutas RESTful para libros y autores
+Route::resource('books', BookController::class);
+Route::resource('authors', AuthorController::class);
+Route::resource('categories', CategoriesController::class);
+Route::get('/miembros', function () {
+    // Obtenemos los primeros 10 miembros con su usuario relacionado
+    $members = Member::with('user')->take(10)->get();
+    return view('temp-members', compact('members'));
 });
